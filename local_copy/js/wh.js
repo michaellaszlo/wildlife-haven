4cd1843604cf7d3afd9837203df8ac2999c3cc87
Michael Laszlo <git@michaellaszlo.com>
2015-10-06 17:36:34 -0400

    In-page editing for other recurring amount

--- local_copy/js/wh.js
+++ local_copy/js/wh.js
@@ -14,6 +14,39 @@ WH.load = function () {
       makeUnselectable(divs[i]);
     }
   }
+  var prepOtherAmount = function (container, prefix, suffix) {
+    var input = container.getElementsByTagName('input')[0],
+        link = container.getElementsByTagName('a')[0],
+        spans = container.getElementsByTagName('span'),
+        amountSpan;
+    for (var i = 0; i < spans.length; ++i) {
+      if (spans[i].className == 'amount') {
+        amountSpan = spans[i];
+        break;
+      }
+    }
+    link.onclick = function () { return false; };
+    container.oninput = container.onkeydown = container.onkeyup = function () {
+      var amount = parseInt(input.value, 10);
+      if (amount !== amount || amount < 1) {
+        amountSpan.innerHTML = '';
+        link.href = '';
+        link.onclick = function () { return false; };
+      } else {
+        amountSpan.innerHTML = amount;
+        link.href = prefix + amount + suffix;
+        link.onclick = function () { return true; };
+      }
+    };
+  };
+  prepOtherAmount(
+      document.getElementById('otherMonthly'),
+      'https://www.paypal.com/cgi-bin/webscr?' + 
+          'cmd=_xclick-subscriptions&business=joyswildlifehaven@gmail.com' +
+          '&currency_code=CAD&lc=CA&a3=',
+      '&p3=12&t3=M&src=1&no_note=1' +
+          '&item_name=Donation%20to%20Wildlife%20Haven%20Waterloo'
+  );
 };
 
 window.onload = WH.load;
